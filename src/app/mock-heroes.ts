ec2dcb96020c4688f33f298421a7b3a7bdb1248e
Chikkelolla Anvesh <33635144+AnveshChikkelolla@users.noreply.github.com>
2019-02-28 11:32:10 +0530

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+      { id: 11, name: 'Mr. Anvesh' },
+      { id: 12, name: 'Rajesh' },
+      { id: 13, name: 'Chandhu' },
+      { id: 14, name: 'Rakesh' },
+      { id: 15, name: 'Raju' },
+      { id: 16, name: 'Shivani' },
+      { id: 17, name: 'Maneesha' },
+      { id: 18, name: 'Teju' },
+      { id: 19, name: 'Divya' },
+      { id: 20, name: 'Pravalika' }
+    ];
+
